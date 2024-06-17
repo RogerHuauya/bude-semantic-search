f3820464300138c2848f993aa8ab70a50b4d359f
Roger <rhuauyam@uni.pe>
2024-06-17 00:21:24 -0500

    webapp added

---
+++ web-app/src/App.jsx
@@ -0,0 +1,26 @@
+import {BrowserRouter, NavLink, Route, Routes} from "react-router-dom";
+import Home from "./Home"
+import Query from "./Query";
+
+function App() {
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <div className="header">
+                    <NavLink exact activeClassName="active" to="/">Home</NavLink>
+                    <NavLink activeClassName="active" to="/query">Query</NavLink>
+                </div>
+                <div className="content">
+                    <Routes>
+                        <Route exac path="/" element={<Home/>}/>
+                        <Route path="/query" element={<Query/>}/>
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        </div>
+    );
+}
+
+export default App;
+
+
